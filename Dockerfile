5fa48f7bae8091807b95a5ac86c52b4327673b21
whitlocktech <whitlocktech@gmail.com>
2023-04-22 03:49:28 -0500

    swagger not working properly

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:19-alpine
 
 LABEL Name="Rust Stats"
 LABEL description="This is will provide a api to get data from the Player Ranks plugin and pull it into a webservice. Designed for Next-Gen Gaming"
-LABEL version="1.1.1"
+LABEL version="1.1.5"
 LABEL maintainer="whitlocktech"
 LABEL maintainer_email="whitlocktech@gmail.com"
 
--- swagger.js
+++ swagger.js
@@ -1,5 +1,17 @@
 const swaggerAutogen = require('swagger-Autogen')()
+
+const doc = {
+    info: {
+        version: '1.1.5',
+        title: 'Next-Gen Gaming Rust Stats',
+        description: 'The stats api for the Player Ranks plugin for the rust game'
+    },
+    host: 'stats.next-gengaming.com',
+    schemes: 'https',
+    basePath:'/v1'
+}
+
 const outputFile = './swagger_output.json'
 const endpointsFile = ['./routes/api']
 
-swaggerAutogen(outputFile, endpointsFile)
+swaggerAutogen(outputFile, endpointsFile, doc)
